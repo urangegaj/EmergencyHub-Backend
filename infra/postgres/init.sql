62d92591e8d5e120651220f093e48f45b40d06a9
Ylli <183634939+Ylli-Berisha@users.noreply.github.com>
2026-05-19 23:15:27 +0200

    feat: scaffold infra

---
+++ infra/postgres/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE auth_db;
+CREATE DATABASE emergency_db;
+CREATE DATABASE police_db;
+CREATE DATABASE medical_db;
+CREATE DATABASE fire_db;
+CREATE DATABASE assessment_db;
+CREATE DATABASE notification_db;
